85fa9b3c73ed07713bcd2c17cc4e96c8ac5415f4
napljagrabio <nap.lagrabio@gmail.com>
2026-02-06 15:20:52 +0800

    fix: error rooms

---
+++ pages/index.js
@@ -0,0 +1,19 @@
+﻿import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>xSync</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <div style={{ width: '100vw', height: '100vh', overflow: 'hidden' }}>
+        <iframe
+          src="/index.html"
+          title="xSync"
+          style={{ border: '0', width: '100%', height: '100%' }}
+        />
+      </div>
+    </>
+  );
+}
